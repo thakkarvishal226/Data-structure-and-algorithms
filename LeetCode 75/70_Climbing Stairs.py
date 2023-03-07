4e575b2f816dd200fa21f358175a143d059c7ee1
= <=>
2023-03-07 19:50:56 +0530

    07-03-2023_office

---
+++ LeetCode 75/70_Climbing Stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one = 1
+        two = 1
+        for _ in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
+
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.climbStairs(5))
---
+++ LeetCode 75/746_Min Cost Climbing Stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minCostClimbingStairs(self, cost) -> int:
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    s = Solution()
+    data = [10,15,20]
+    print(s.minCostClimbingStairs(data))
