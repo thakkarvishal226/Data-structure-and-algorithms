ae614cc3026e90df10eaab5797d2bd5dd00a4f16
= <=>
2023-03-10 17:19:36 +0530

    10-03-2023_office

--- LeetCode 75/746_Min Cost Climbing Stairs.py
+++ LeetCode 75/746_Min Cost Climbing Stairs.py
@@ -1,6 +1,11 @@
 class Solution:
     def minCostClimbingStairs(self, cost) -> int:
-        pass
+        cost.append(0)
+
+        for i in range(len(cost) - 3,-1,-1):
+            cost[i] = min(cost[i] + cost[i+1],cost[i] + cost[i+2])
+        
+        return min(cost[0], cost[1])
 
 
 
@@ -8,5 +13,5 @@ class Solution:
 
 if __name__ == "__main__":
     s = Solution()
-    data = [10,15,20]
+    data = [1,100,1,1,1,100,1,1,100,1]
     print(s.minCostClimbingStairs(data))
