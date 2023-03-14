96949c347bcb33b9fbaf563b3a6010e79d0e5224
= <=>
2023-03-14 20:17:48 +0530

    14_03_2023_the_office_2

--- LeetCode 75/62_Unique Paths.py
+++ LeetCode 75/62_Unique Paths.py
@@ -2,29 +2,19 @@
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        queue = [(0,0)]
-        count = 0
-        if m > 1 and n >1:
-            while queue:
-                x, y = queue.pop()
-                if (x >=0 and y >= 0) and (x < m and y < n):
-                    if x == m-1 and y+1 == n-1:
-                        count+=1
-                    elif x+1 == m-1 and y == n-1:
-                        count += 1
-                    else:
-                        if (x+1,y) not in queue:
-                            queue.append((x+1,y))
-                        if (x,y+1) not in queue:
-                            queue.append((x,y+1))
-        else:
-            count += 1
-        return count
-
+        row = [1] * n
+        print(row)
+        for _ in range(m-1):
+            newrow = [1] * n
+            for j in range(n-2,-1,-1):
+                newrow[j] = row[j] + newrow[j+1]
+            row = newrow
+            print(row)
+        return row[0]
 
 
 if __name__ == "__main__":
     s = Solution()
-    grid_size = (23,12)
+    grid_size = (30,12)
     
     print(s.uniquePaths(grid_size[0],grid_size[1]))
